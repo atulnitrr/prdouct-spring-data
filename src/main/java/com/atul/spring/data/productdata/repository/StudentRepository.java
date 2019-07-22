eb02b430220cef1975c323f46a8e9322e586b8de
atulnitrr <atulkumar458@gmail.com>
2019-07-22 20:34:35 +0530

    Added jpql

--- src/main/java/com/atul/spring/data/productdata/repository/StudentRepository.java
+++ src/main/java/com/atul/spring/data/productdata/repository/StudentRepository.java
@@ -1,12 +1,37 @@
 package com.atul.spring.data.productdata.repository;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.atul.spring.data.productdata.entity.StudentEntity;
 
 
 @Repository
 public interface StudentRepository extends PagingAndSortingRepository<StudentEntity, Long> {
+
+    @Query(value = "select student from StudentEntity student")
+    List<StudentEntity> findAllStudents();
+
+    @Query(value = "select s.firstName , s.lastName from StudentEntity s")
+    List<Object[]> findStudentspartial();
+
+    @Query(value = "select s from StudentEntity s where s.firstName = :firstName")
+    List<StudentEntity> findAllStudentByFirstName(@Param("firstName") final String firstName);
+
+
+    @Query(value = "select s from StudentEntity s where s.score >:minScore and s.score <:maxScore")
+    List<StudentEntity> findAllInGivenScore(@Param("minScore") final int minScore,
+            @Param("maxScore") final int maxScore);
+
+    @Modifying
+    @Query(value = "delete from StudentEntity s where s.firstName = :firstName")
+    void deletStudentsByFirstName(@Param("firstName") final String firstName);
+
+
+
 
 
 }
--- src/test/java/com/atul/spring/data/productdata/repository/StudentRepositoryTest.java
+++ src/test/java/com/atul/spring/data/productdata/repository/StudentRepositoryTest.java
@@ -1,11 +1,14 @@
 package com.atul.spring.data.productdata.repository;
 
 import static org.junit.Assert.*;
+import java.util.List;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
 import com.atul.spring.data.productdata.entity.StudentEntity;
 
 
@@ -23,5 +26,43 @@ public class StudentRepositoryTest {
         studentEntity.setLastName("gates");
         studentEntity.setScore(98);
         studentRepository.save(studentEntity);
+    }
+
+    @Test
+    public void test_GetALlSql() {
+        studentRepository.findAllStudents().forEach(studentEntity ->
+                System.out.println(studentEntity));
+
+    }
+
+    @Test
+    public void test_GetALlPartial() {
+        List<Object[]> list = studentRepository.findStudentspartial();
+        for (Object[] objects : list) {
+            System.out.println(objects[0]);
+            System.out.println(objects[1]);
+        }
+
+    }
+
+    @Test
+    public void test_findByTest() {
+        List<StudentEntity> list = studentRepository.findAllStudentByFirstName("bill");
+        System.out.println(list);
+    }
+
+    @Test
+    public void test_findAllStudentByScores() {
+        System.out.println(studentRepository.findAllInGivenScore(55, 90));
+    }
+
+    @Test
+    @Transactional
+    // only do this in UTs
+    @Rollback(false)
+    //
+    public void test_DeleteStudentByFirstName() {
+        studentRepository.deletStudentsByFirstName("ak1");
+
     }
 }
