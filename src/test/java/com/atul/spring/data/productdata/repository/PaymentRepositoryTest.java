6487d2807e15bde62b4001ff6a819618a9270002
atulnitrr <atulkumar458@gmail.com>
2019-07-23 22:37:31 +0530

    Added check test

--- src/test/java/com/atul/spring/data/productdata/repository/PaymentRepositoryTest.java
+++ src/test/java/com/atul/spring/data/productdata/repository/PaymentRepositoryTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.atul.spring.data.productdata.entity.CheckEntity;
 import com.atul.spring.data.productdata.entity.CreditCardEntity;
 
 
@@ -19,12 +20,21 @@ public class PaymentRepositoryTest {
 
     @Test
     public void test_creditcardPaymet() {
-
         final CreditCardEntity creditCardEntity = new CreditCardEntity();
         creditCardEntity.setId(123);
         creditCardEntity.setAmount(89);
         creditCardEntity.setCardnumber("12345342");
         paymentRepository.save(creditCardEntity);
+
+    }
+
+    @Test
+    public void test_checkPaymet() {
+        final CheckEntity checkEntity = new CheckEntity();
+        checkEntity.setId(1243);
+        checkEntity.setAmount(89);
+        checkEntity.setChecknumber("3455677");
+        paymentRepository.save(checkEntity);
 
     }
 }
