1094e6bb6f1ba813cad7c6afada89a7aec240d1a
atulnitrr <atulkumar458@gmail.com>
2019-07-24 22:02:54 +0530

    Added caching

--- src/main/java/com/atul/spring/data/productdata/entity/StudentEntity.java
+++ src/main/java/com/atul/spring/data/productdata/entity/StudentEntity.java
@@ -1,16 +1,24 @@
 package com.atul.spring.data.productdata.entity;
 
+import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
 
 
 @Entity
 @Table(name = "student")
-public class StudentEntity {
+//this is used for caching
+@Cache(usage = CacheConcurrencyStrategy.READ_ONLY)
+public class StudentEntity implements Serializable {
+
+    private static final long serialVersionUID = 1l;
+
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
